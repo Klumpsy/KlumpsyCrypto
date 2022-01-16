d6c0f67280771772365c66b32ef39b02aa5108f5
bart <bart_klumperman@Live.nl>
2022-01-16 15:35:14 +0100

    added news query option

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useGetCryptoNewsQuery } from '../../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../../services/cryptoApi';
+
 import moment from 'moment';
 
 import "./news.css"
 
 const News = ({ simplified }) => {
-    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency", count: simplified ? 10 : 100 });
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: newsCategory, count: simplified ? 6 : 100 });
+    const { data } = useGetCryptosQuery(100);
 
     if (!cryptoNews?.value) return "Loading..."
 
     return (
         <>
+            {!simplified && (
+                <select
+                    className='news-select-menu'
+                    placeholder="Select crypto"
+                    onChange={(e) => setNewsCategory(e.target.value)}
+                >
+                    <option value="Cryptocurrency" key="Cryptocurrency">Cryptocurrency</option>
+                    {
+                        data?.data?.coins?.map(crypto => (<option key={crypto.name} value={crypto.name}>{crypto.name}</option>))
+                    }
+                </select>
+            )
+            }
             <div className="news-wrapper">
                 {cryptoNews.value.map(newsItem => (
                     <div className="news-item-container" key={newsItem.name}>
@@ -26,7 +43,7 @@ const News = ({ simplified }) => {
                         </div>
                         <div className="news-item-link">
                             <img className="news-avatar" src={newsItem?.provider[0]?.image?.thumbnail?.contentUrl} />
-                            <p>{moment(newsItem.datePublished).startOf('ss').fromNow()}</p>
+                            <p className="news-time-stamp">{moment(newsItem.datePublished).startOf('ss').fromNow()}</p>
                             <a href={newsItem.url} target="_blank">Read more</a>
                         </div>
                     </div>
